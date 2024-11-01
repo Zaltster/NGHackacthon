2812161320314e2c39ce59b815d7b15669191430
Samuel Shvartsman <86971136+Zaltster@users.noreply.github.com>
2024-10-31 20:26:00 -0400

    Update robotheflow.py
    API key issues

--- robotheflow.py
+++ robotheflow.py
@@ -2,7 +2,7 @@ from inference_sdk import InferenceHTTPClient
 
 CLIENT = InferenceHTTPClient(
     api_url="https://detect.roboflow.com",
-    api_key="fCuadHZbBFPgDvfQ0w6j"
+    api_key="INSERT API KEY HERE"
 )
 
 result = CLIENT.infer("image1.jpg", model_id="ng-object-detection/3")
